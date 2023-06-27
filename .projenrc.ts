6292a9c8efd5d1d252a637092b858add844b125c
yicr <yicr@users.noreply.github.com>
2023-06-27 11:38:15 +0900

    chore(deps): add package with cdk version to 2.80.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,21 @@
-import { awscdk } from 'projen';
+import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.42.0',
+  cdkVersion: '2.80.0',
   defaultReleaseBranch: 'main',
   name: 'aws-secure-flow-log-bucket',
+  keywords: ['aws', 'cdk', 'aws-cdk', 's3', 'bucket', 'vpc', 'flow'],
   projenrcTs: true,
   repositoryUrl: 'https://github.com/yicr/aws-secure-flow-log-bucket.git',
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  deps: [
+    '@yicr/aws-secure-log-bucket',
+  ],
+  peerDeps: [
+    '@yicr/aws-secure-log-bucket',
+  ],
+  minNodeVersion: '16.0.0',
+  workflowNodeVersion: '16.19.1',
 });
 project.synth();
