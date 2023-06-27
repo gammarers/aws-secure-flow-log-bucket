662da3598f31ecfef3a072a86bd616b63c8eb5fe
yicr <yicr@users.noreply.github.com>
2023-06-27 14:41:08 +0900

    feat!: first happy code with test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { SecureLogBucket, SecureLogBucketProps } from '@yicr/aws-secure-log-bucket';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export interface SecureFlowLogBucketProps extends SecureLogBucketProps {
+  readonly keyPrefixes?: string[];
+}
+
+export class SecureFlowLogBucket extends SecureLogBucket {
+  constructor(scope: Construct, id: string, props?: SecureFlowLogBucketProps) {
+    super(scope, id, props);
+
+    // 👇Get current account
+    const account = cdk.Stack.of(this).account;
+
+    // 👇バケットACLアクセス権
+    this.addToResourcePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:GetBucketAcl'],
+      principals: [
+        new iam.ServicePrincipal('delivery.logs.amazonaws.com'),
+      ],
+      resources: [this.bucketArn],
+    }));
+
+    // 👇バケット書き込みアクセス権
+    this.addToResourcePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:PutObject'],
+      principals: [
+        new iam.ServicePrincipal('delivery.logs.amazonaws.com'),
+      ],
+      //resources: [`${this.bucketArn}/AWSLogs/${account}/*`],
+      resources: (() => {
+        if (props?.keyPrefixes && props?.keyPrefixes.length > 0) {
+          const resources: Array<string> = [];
+          for (const keyPrefix of props.keyPrefixes) {
+            resources.push(`${this.bucketArn}/${keyPrefix}/AWSLogs/${account}/*`);
+          }
+          return resources;
+        }
+        return [`${this.bucketArn}/AWSLogs/${account}/*`];
+      })(),
+      conditions: {
+        StringEquals: {
+          's3:x-amz-acl': 'bucket-owner-full-control',
+        },
+      },
+    }));
+
   }
 }
---
+++ test/bucket-assign-key-prefix.test.ts
@@ -0,0 +1,130 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { SecureFlowLogBucket } from '../src';
+
+describe('SecureFlowLogBucket Testing', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'TestingStack', {
+    env: {
+      account: '123456789012',
+      region: 'us-east-1',
+    },
+  });
+
+  const keyPrefixes = [
+    'example-prefix-a',
+    'example-prefix-b',
+  ];
+
+  const bucket = new SecureFlowLogBucket(stack, 'SecureFlowLogBucket', {
+    keyPrefixes,
+  });
+
+  it('Is Bucket', () => {
+    expect(bucket).toBeInstanceOf(s3.Bucket);
+  });
+
+  const template = Template.fromStack(stack);
+
+  it('Should have encrypted by aws kms', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      BucketEncryption: Match.objectEquals({
+        ServerSideEncryptionConfiguration: [
+          {
+            ServerSideEncryptionByDefault: {
+              SSEAlgorithm: 'aws:kms',
+            },
+          },
+        ],
+      }),
+    });
+  });
+
+  it('Should have acl access bucket policy', () => {
+    template.hasResourceProperties('AWS::S3::BucketPolicy', Match.objectEquals({
+      Bucket: {
+        Ref: Match.stringLikeRegexp('SecureFlowLogBucket'),
+      },
+      PolicyDocument: {
+        Version: '2012-10-17',
+        Statement: Match.arrayWith([
+          Match.objectEquals({
+            Action: 's3:GetBucketAcl',
+            Effect: 'Allow',
+            Principal: {
+              Service: 'delivery.logs.amazonaws.com',
+            },
+            Resource: {
+              'Fn::GetAtt': [
+                Match.stringLikeRegexp('SecureFlowLogBucket'),
+                'Arn',
+              ],
+            },
+          }),
+        ]),
+      },
+    }));
+  });
+
+  it('Should have write access bucket policy', () => {
+    template.hasResourceProperties('AWS::S3::BucketPolicy', Match.objectEquals({
+      Bucket: {
+        Ref: Match.stringLikeRegexp('SecureFlowLogBucket'),
+      },
+      PolicyDocument: {
+        Version: '2012-10-17',
+        Statement: Match.arrayWith([
+          Match.objectEquals({
+            Action: 's3:PutObject',
+            Effect: 'Allow',
+            Principal: {
+              Service: 'delivery.logs.amazonaws.com',
+            },
+            Resource: [
+              {
+                'Fn::Join': [
+                  '',
+                  [
+                    {
+                      'Fn::GetAtt': [
+                        Match.stringLikeRegexp('SecureFlowLogBucket'),
+                        'Arn',
+                      ],
+                    },
+                    '/example-prefix-a/AWSLogs/123456789012/*',
+                  ],
+                ],
+              },
+              {
+                'Fn::Join': [
+                  '',
+                  [
+                    {
+                      'Fn::GetAtt': [
+                        Match.stringLikeRegexp('SecureFlowLogBucket'),
+                        'Arn',
+                      ],
+                    },
+                    '/example-prefix-b/AWSLogs/123456789012/*',
+                  ],
+                ],
+              },
+            ],
+            Condition: {
+              StringEquals: {
+                's3:x-amz-acl': 'bucket-owner-full-control',
+              },
+            },
+          }),
+        ]),
+      },
+    }));
+  });
+
+  it('Should match snapshot', () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+});
