2ed95cbb345bb6e7bb11473179b9f3a496871126
yicr <yicr@users.noreply.github.com>
2023-08-24 15:52:56 +0900

    fix!: jsii version to ~5.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
   typescriptVersion: '4.9.x',
+  jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-secure-flow-log-bucket',
   description: 'Specific AWS VPC FlowLog Bucket',
