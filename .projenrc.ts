570daede176c7aaf88a0454a664c28182ff2fc90
yicr <yicr@users.noreply.github.com>
2024-07-20 16:27:25 +0530

    chore(deps): upgarde main with workflow node version 22.4.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -24,7 +24,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@gammarers/aws-secure-bucket@~1.4.1',
   ],
   minNodeVersion: '16.0.0',
-  workflowNodeVersion: '22.x',
+  workflowNodeVersion: '22.4.x',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
